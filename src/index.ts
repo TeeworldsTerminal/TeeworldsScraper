46b95dd910e91a0328551292b9f97fd40c640ea3
WocketSesh <jackcommon1@outlook.com>
2023-07-19 12:18:12 +0100

    basic gores player stats

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,21 @@ export { WebScraper };
 let scraper = new WebScraper();
 
 async function main() {
-  await scraper.gores.getPlayerStats("Wocket Woo");
+  let name = process.argv[2];
+
+  if (!name) {
+    console.log(`Provide a name`);
+    process.exit();
+  }
+  let stats = await scraper.gores.getPlayerStats(name);
+
+  console.log(
+    `${name}'s Gores Stats\n\nPoints: ${stats.totalPoints} (Fixed: ${stats.fixed
+    } | Seasonal: ${stats.season})\nRank: ${stats.rank
+    }\n\nFavourite Partners:\n${stats.topPartners
+      .map((x) => `${x.name} (${x.finishes} finishes)`)
+      .join("\n")}\n\n"Wasted" Time Spent On Maps: ${stats.wastedTime}`
+  );
 }
 
 main();
--- src/scrapers/GoresScraper.ts
+++ src/scrapers/GoresScraper.ts
@@ -25,13 +25,16 @@ export class GoresScraper {
 
   async getPlayerStats(playerName: string, data?: any) {
     let $ = cheerio.load(data || (await this.getPlayerPage(playerName)));
+    let row = $("div#content > div.container > div.row");
 
-    let content = $("div#content > div.container > div.row > div.col");
+    let content = cheerio.load(row.html() as string)("div.col");
 
     let pointsCard = cheerio
       .load(content.html() as string)("div.card")
       .eq(0)
       .text();
+
+    // TODO: do this shit later
     let otherCard = cheerio
       .load(content.html() as string)("div.card")
       .eq(1)
@@ -50,12 +53,50 @@ export class GoresScraper {
     fixed = fixed.split(":")[1].trim();
     season = season.split(":")[1].trim();
 
+    let tabContent = cheerio.load(row.html() as string)(
+      "div.col.col-sm-12 > div.tab-content"
+    );
+
+    let mapData = cheerio.load(tabContent.html() as string)("div#nav-maps");
+    let generalData = cheerio.load(tabContent.html() as string)(
+      "div#nav-general"
+    );
+
+    let cleaned = generalData
+      .text()
+      .split("\n")
+      .map((x) => x.trim())
+      .filter((x) => x.trim().length > 0);
+
+    // Just remove useless text that isnt needed
+    cleaned.splice(0, 1);
+    cleaned.splice(cleaned.length - 1, 1);
+
+    let topPartners: { finishes: number; name: string }[] = [];
+
+    for (let i = 0; i < cleaned.length; i++) {
+      let curr = cleaned[i];
+
+      if (curr.startsWith("Wasted")) break;
+
+      let split = curr.split(" ");
+
+      topPartners.push({
+        finishes: parseInt(split[0]),
+        name: split.slice(1).join(" "),
+      });
+    }
+
+    let wastedTime = cleaned[cleaned.length - 1];
+
     return {
       name,
       rank: pointRank,
       totalPoints,
       fixed,
       season,
+      topPartners,
+      wastedTime,
     };
   }
 }
